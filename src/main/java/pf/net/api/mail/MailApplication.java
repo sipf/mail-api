9b64f3c9179632d7d8f79eb13ceab2a8ae7f0634
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2017-08-16 17:36:52 -1000

    Externalize Async configuration

--- src/main/java/pf/net/api/mail/MailApplication.java
+++ src/main/java/pf/net/api/mail/MailApplication.java
@@ -2,13 +2,12 @@ package pf.net.api.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class MailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MailApplication.class, args);
 	}
+
 }
